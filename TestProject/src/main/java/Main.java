17bd989646292d294ca5b368b10c32c9630d0782
Naneth-S <43665451+Naneth-S@users.noreply.github.com>
2020-05-25 20:13:46 +1000

    in Main.java: import, inner class, fan-in fan-out

--- TestProject/src/main/java/Main.java
+++ TestProject/src/main/java/Main.java
@@ -12,9 +12,19 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import java.util.ArrayList;
+
 public class Main {
 
     private static final String filePath = "src/test/java/Tree_TestData/src/main.java";
+    private static List<FanInOutMethod> methodsList = new ArrayList<FanInOutMethod>();
 
     private static List<String> readFileInList(String fileName)
     {
@@ -28,6 +38,40 @@ public class Main {
         return lineList;
     }
 
+    //Naneth: This inner class separates the methods of one class and puts the methods in a static array list
+    private static class MethodSplitter extends VoidVisitorAdapter<Void> {
+
+        @Override
+        public void visit(MethodDeclaration md, Void arg){
+            super.visit(md, arg);
+
+            //create new FanInOutMethod Object
+            FanInOutMethod temp = new FanInOutMethod();
+
+            //update values of FanInOutMethod object
+            temp.setMethodName(md.getNameAsString());
+            temp.setMethodBlock(md.toString());
+
+            //prepare parser
+            TypeSolver typeSolver = new ReflectionTypeSolver();
+            JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+            StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+
+            //parse input
+            //add dummy class declaration to surround method coz it won't compile code with error
+            CompilationUnit cu2 = StaticJavaParser.parse("public class Dummy {" + temp.getMethodBlock() + "}");
+
+            //visit each method
+            cu2.findAll(MethodCallExpr.class).forEach(mce -> {
+                //save the method name to the FanInMethod object's ArrayList of method calls
+                temp.getCalledMethodsList().add(mce.getNameAsString());
+            });
+
+            //add temp to methodsList
+            methodsList.add(temp);
+        }
+    }
+    
     public static void main(String[] args) throws FileNotFoundException {
 
         CompilationUnit cu = StaticJavaParser.parse(new File(filePath));
@@ -45,5 +89,43 @@ public class Main {
 
         Tree t = new Tree(cu);
         t.result();
+        
+        //////////////////////////////////////( Fan-in/out: starts )/////////////////////////////////////////
+        //path
+        String s2 = "src5";
+        //array list of compilation units
+        List<CompilationUnit> allCU;
+
+        //create a new parser
+        Parser rootParser = new Parser();
+
+        //initialise allCU
+        allCU = rootParser.getCompilationUnits(s2);
+
+        //iterate on each compilation units
+        for(int i = 0; i < allCU.size(); i++){
+            //now we want to separate each methods and save them in a FanInOutMethod object
+
+            //create visitor for normal methods
+            VoidVisitor<?> methodVisitor = new MethodSplitter();
+            methodVisitor.visit(allCU.get(i), null);
+        }
+
+        //*****************************( fan-out )*****************************//
+        //make new FanOut object
+        FanOut fo = new FanOut();
+
+        //execute fan out method of FanOut object
+        fo.calculateFanOut(methodsList);
+
+        //*****************************( fan-in )*****************************//
+        //make new FanIn object
+        FanIn fi = new FanIn();
+
+        //execute fan in method of FanIn object
+        fi.calculateFanIn(methodsList);
+
+        ////////////////////////////////////( Fan-in/out: finished )///////////////////////////////////////
+
     }
 }
