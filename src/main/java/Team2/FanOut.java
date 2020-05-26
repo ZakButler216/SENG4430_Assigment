d422452e388039cd30f5018edf5c3c763b693b5a
Naneth-S <43665451+Naneth-S@users.noreply.github.com>
2020-05-26 17:36:42 +1000

    Update FanOut.java

--- src/main/java/Team2/FanOut.java
+++ src/main/java/Team2/FanOut.java
@@ -34,36 +34,58 @@
 
 package Team2;
 
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class FanOut {
     public FanOut() {
     }
 
-    public void calculateFanOut(List<FanInOutMethod> methodsList){
-        System.out.println("///////////////////////////////////////////( fan-out result )/////////////////////////////////////////////");
-        System.out.println("It is ideal to keep the fan-out value on average or below average. The lower the better for code reusability.");
-        System.out.println("");
-
-        System.out.println("//****************************( summary )****************************//");
+    public List<Integer> calculateFanOut(List<FanInOutMethod> methodsList){
+        //populate this int array list with; # total called methods, and average fan out value
+        List<Integer> result = new ArrayList<>();
         int totalCalledMethods = 0;
-        for(int a = 0; a < methodsList.size(); a++){
-            totalCalledMethods += methodsList.get(a).getCalledMethodsList().size();
-        }
-        System.out.println("Total number of called methods: " + totalCalledMethods);
+        int averageFanOut = 0;
 
-        //avoid division by zero
-        if(methodsList.size() > 0){
-            int averageFanOut = totalCalledMethods/methodsList.size();
-            System.out.println("Average fan-out value: " + averageFanOut);
+        //if the file is invalid/ has code errors, or or there are no methods
+        if(methodsList.size() <= 0){
+            System.out.println( "No methods detected. \n" +
+                                "Please ensure that the Java file/s have no error and that \n" +
+                                "there is at least one method in the Java file/s.");
+            System.out.println("");
+        }
+        else{
+            System.out.println("///////////////////////////////////////////( fan-out result )/////////////////////////////////////////////");
+            System.out.println("It is ideal to keep the fan-out value on average or below average. The lower the better for code reusability.");
+            System.out.println("");
+
+            System.out.println("//****************************( summary )****************************//");
+
+            for(int a = 0; a < methodsList.size(); a++){
+                totalCalledMethods += methodsList.get(a).getCalledMethodsList().size();
+            }
+            System.out.println("Total number of called methods: " + totalCalledMethods);
+
+            //avoid division by zero
+            if(methodsList.size() > 0){
+                averageFanOut = totalCalledMethods/methodsList.size();
+                System.out.println("Average fan-out value: " + averageFanOut);
+                System.out.println("");
+            }
+
+            System.out.println("//****************************( detailed )****************************//");
+            for(int b = 0; b < methodsList.size(); b++){
+                System.out.println("Method Name: " + methodsList.get(b).getMethodName());
+                System.out.println("Number of methods called: " + methodsList.get(b).getCalledMethodsList().size() + "\n");
+            }
             System.out.println("");
         }
 
-        System.out.println("//****************************( detailed )****************************//");
-        for(int b = 0; b < methodsList.size(); b++){
-            System.out.println("Method Name: " + methodsList.get(b).getMethodName());
-            System.out.println("Number of methods called: " + methodsList.get(b).getCalledMethodsList().size() + "\n");
-        }
-        System.out.println("");
+        result.add(totalCalledMethods);
+        result.add(averageFanOut);
+        return result;
     }
 }
