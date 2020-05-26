1f92a23aab65af3fa3fd308f4a7c1ee5900c1bbb
Naneth-S <43665451+Naneth-S@users.noreply.github.com>
2020-05-26 17:23:10 +1000

    Added: test source files & module test classes

---
+++ TestProject/srcInvalidFile/main/java/Team2/FanIn.java
@@ -0,0 +1,5 @@
+
+
+public void invalidFile(){
+    System.out.print("");
+}
---
+++ TestProject/srcValid/main/java/Three.java
@@ -0,0 +1,19 @@
+/*
+ * File name: Three.java
+ * Author:       Naneth Sayao
+ * Date:         25 May 2020
+ * Version:      1.0
+ * Description:  A test file
+ *                   - One.java will call a method from Two.java
+ *                   - Two.java will call a method from Three.java
+ *                   - Three.java will call a method from One.java
+ * */
+public class Three {
+    public Three() {
+    }
+
+    public void threeMethod(){
+        One uno = new One();
+        uno.oneMethod();
+    }
+}
