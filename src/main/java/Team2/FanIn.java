e9256478293ca6c7d015cebd1bc0dedc735ae376
Naneth-S <43665451+Naneth-S@users.noreply.github.com>
2020-05-26 17:36:15 +1000

    Update FanIn.java

--- src/main/java/Team2/FanIn.java
+++ src/main/java/Team2/FanIn.java
@@ -36,6 +36,7 @@
 
 package Team2;
 
+import java.util.ArrayList;
 import java.util.List;
 import Team2.FanInOutMethod;
 
@@ -44,71 +45,92 @@ public class FanIn {
     public FanIn() {
     }
 
-    public void calculateFanIn(List<FanInOutMethod> methodsList){
-        System.out.println("///////////////////////////////////////////( fan-in result )/////////////////////////////////////////////");
-        System.out.println("Zero caller may or may not indicate a dead method code.");
-        System.out.println("One caller makes the method a candidate to be moved inside the caller method for optimisation.");
-        System.out.println("Two or more callers indicate reuse.");
-        System.out.println("A high the number of callers may mean: \n - good for reuse \n - bad for cross-file coupling \n");
-
-        System.out.println("//****************************( summary )****************************//");
+    public List<Integer> calculateFanIn(List<FanInOutMethod> methodsList){
+        /*
+        * populate this int array list with;
+        *   # total methods,
+        *   # methods that called a method,
+        *   # possibly dead methods,
+        *   # transferable methods, and
+        *   # reused methods.
+        * */
+        List<Integer> result = new ArrayList<>();
         int methodCallerCount = 0;
         int oneCaller = 0;
         int twoOrMoreCaller = 0;
-        for(int s = 0; s < methodsList.size(); s++){
-            //if called methods ArrayList size is greater than zero, increment methodCallerCount
-            if(methodsList.get(s).getCalledMethodsList().size() > 0){
-                methodCallerCount++;
-            }
+        int deadMethods = 0;
 
-            //see if method has 1 or more than 2 callers
-            if(methodsList.get(s).getCalledMethodsList().size() == 1){
-                //if only 1 caller
-                oneCaller++;
-            }
-            else if(methodsList.get(s).getCalledMethodsList().size() >= 2){
-                twoOrMoreCaller++;
-            }
+        //if the file is invalid/ has code errors, or or there are no methods
+        if(methodsList.size() > 0){
+            System.out.println("///////////////////////////////////////////( fan-in result )/////////////////////////////////////////////");
+            System.out.println("Zero caller may or may not indicate a dead method code.");
+            System.out.println("One caller makes the method a candidate to be moved inside the caller method for optimisation.");
+            System.out.println("Two or more callers indicate reuse.");
+            System.out.println("A high the number of callers may mean: \n - good for reuse \n - bad for cross-file coupling \n");
 
-            //how many called a specific method?
-            //save the name of this current method
-            String name = methodsList.get(s).getMethodName();
-            //iterate each method in methodsList
-            for(int i = 0; i < methodsList.size(); i++){
-                //and then iterate again on their called methods list
-                for(int j = 0; j < methodsList.get(i).getCalledMethodsList().size(); j++){
-                    //compare the name of this method and the called methods
-                    if(name.equals(methodsList.get(i).getCalledMethodsList().get(j))){
-                        //if there is a match, save the caller name on the callerList of the FanInOutMethod object of this method
-                        methodsList.get(i).getCallerList().add(methodsList.get(i).getCalledMethodsList().get(j));
-                    }
+            System.out.println("//****************************( summary )****************************//");
 
+            for(int s = 0; s < methodsList.size(); s++){
+                //if called methods ArrayList size is greater than zero, increment methodCallerCount
+                if(methodsList.get(s).getCalledMethodsList().size() > 0){
+                    methodCallerCount++;
                 }
+
+                //see if method has 1 or more than 2 callers
+                if(methodsList.get(s).getCalledMethodsList().size() == 1){
+                    //if only 1 caller
+                    oneCaller++;
+                }
+                else if(methodsList.get(s).getCalledMethodsList().size() >= 2){
+                    twoOrMoreCaller++;
+                }
+
+                //how many called a specific method?
+                //save the name of this current method
+                String name = methodsList.get(s).getMethodName();
+                //iterate each method in methodsList
+                for(int i = 0; i < methodsList.size(); i++){
+                    //and then iterate again on their called methods list
+                    for(int j = 0; j < methodsList.get(i).getCalledMethodsList().size(); j++){
+                        //compare the name of this method and the called methods
+                        if(name.equals(methodsList.get(i).getCalledMethodsList().get(j))){
+                            //if there is a match, save the caller name on the callerList of the FanInOutMethod object of this method
+                            methodsList.get(i).getCallerList().add(methodsList.get(i).getCalledMethodsList().get(j));
+                        }
+
+                    }
+                }
+
             }
 
-        }
+            System.out.println("Total Number of methods: " + methodsList.size());
+            System.out.println("Number of methods that called a method: " + methodCallerCount);
+            deadMethods = methodsList.size() - methodCallerCount;
+            System.out.println("Number of possibly dead methods: " + deadMethods);
+            System.out.println("Number of methods that can be transferred to the caller: " + oneCaller);
+            System.out.println("Number of reused methods: " + twoOrMoreCaller + "\n");
+            System.out.println("//****************************( detailed )****************************//");
 
-        System.out.println("Total Number of methods: " + methodsList.size());
-        System.out.println("Number of methods that called a method: " + methodCallerCount);
-        int deadMethods = methodsList.size() - methodCallerCount;
-        System.out.println("Number of possibly dead methods: " + deadMethods);
-        System.out.println("Number of methods that can be transferred to the caller: " + oneCaller);
-        System.out.println("Number of reused methods: " + twoOrMoreCaller + "\n");
-        System.out.println("//****************************( detailed )****************************//");
+            //iterate on the methods list and print how many callers each method has
+            for(int k = 0; k < methodsList.size(); k++){
+                System.out.println("Method Name: " + methodsList.get(k).getMethodName());
+                System.out.println("Number of callers of " + methodsList.get(k).getMethodName() + ": " + methodsList.get(k).getCallerList().size());
 
-        //iterate on the methods list and print how many callers each method has
-        for(int k = 0; k < methodsList.size(); k++){
-            System.out.println("Method Name: " + methodsList.get(k).getMethodName());
-            System.out.println("Number of callers of " + methodsList.get(k).getMethodName() + ": " + methodsList.get(k).getCallerList().size());
+                for(int d = 0; d < methodsList.get(k).getCallerList().size(); d++){
+                    System.out.println(" - " + methodsList.get(k).getCallerList().get(d));
+                }
 
-            for(int d = 0; d < methodsList.get(k).getCallerList().size(); d++){
-                System.out.println(" - " + methodsList.get(k).getCallerList().get(d));
+                System.out.println("");
             }
 
             System.out.println("");
         }
 
-        System.out.println("");
-
+        result.add(methodsList.size());
+        result.add(methodCallerCount);
+        result.add(deadMethods);
+        result.add(oneCaller);
+        result.add(twoOrMoreCaller);
+        return result;
     }
 }
