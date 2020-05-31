7fe5e5885daa1b33cc695c573d258d7ec159cd75
GeethW <geethma.wijenayake@yahoo.com>
2020-05-31 15:17:01 +0800

    finished program size

--- TestProject/src/main/java/Main.java
+++ TestProject/src/main/java/Main.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import main.java.coupling.Coupling;
+import main.java.programSize.ProgramSize;
 import main.java.coupling.tests.Node;
 
 public class Main {
@@ -62,114 +63,10 @@ public class Main {
         Coupling coupling = new Coupling(cuList, "");
         String results = coupling.getResults();
         System.out.println(results);
+
+        ProgramSize progSize = new ProgramSize(cuList, "");
+        String sizeResults = progSize.getResults();
+        System.out.println(sizeResults);
         //coupling.showResult();
-
-
-        /*
-        for(CompilationUnit cu : cuList){
-            System.out.println(cu.getPrimaryTypeName());
-        }
-        */
     }
 }
-
-/*
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-import main.java.coupling.Coupling;
-
-
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.resolution.types.ResolvedType;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
-import java.net.URISyntaxException;
-
-public class Main {
-
-    private static final String filePath = "C:\\Users\\geeth\\OneDrive\\Documents\\SQ proj\\SENG4430_Assigment\\TestProject\\src\\main\\java\\coupling\\tests\\Interface.java"; //"src/test/java/Tree_TestData/src/main.java";
-
-    private static List<String> readFileInList(String fileName)
-    {
-        List<String> lineList = Collections.emptyList();
-        try
-        {
-            lineList = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        return lineList;
-    }
-
-    public static void main(String[] args) throws FileNotFoundException {*/
-        /*if(args.length>0)
-        {
-            try {
-                filePath = args[0];
-                File file = new File(filePath);
-            } catch(Exception e)
-            {
-                System.out.println("Something went wrong when parsing file.");
-                System.out.println(e.printStackTrace());
-            }
-        }  else {
-            System.out.println("Enter file name.");
-        }*/
-/*
-        File file = new File(filePath);
-        CompilationUnit cu = StaticJavaParser.parse(file);
-        List<String> lineList = readFileInList(filePath);
-
-        // These solvers will also parse the other files around the given file as well as classes in the current JVM
-        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
-        combinedTypeSolver.add(new ReflectionTypeSolver());
-        combinedTypeSolver.add(new JavaParserTypeSolver(file.getParentFile()));
-
-        try {
-            combinedTypeSolver.add(new JarTypeSolver(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath()));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
-        //ResolvedType rt = JavaParserFacade.get(typeSolver).convertToUsage(type);
-
-        // Configure JavaParser to use type resolution
-        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
-        StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
-
-        CompilationUnit newCu = StaticJavaParser.parse(new File(filePath));
-*/
-        /*
-        System.out.println("Fog Index Results:");
-        FogIndex fogIndex = new FogIndex(lineList);
-        fogIndex.showResults();
-
-        System.out.println("\n");
-         */
-        //System.out.println("Response For a Class Results");
-
-        /*
-        RFC rfc = new RFC(cu);
-        rfc.showResults();
-
-        Tree t = new Tree(cu);
-        t.result();
-
-
-        Coupling coupling = new Coupling(newCu);
-        //coupling.showResult();
-    }
-}*/
---
+++ TestProject/src/main/java/programSize/ProgramSize.java
@@ -0,0 +1,153 @@
+package main.java.programSize;
+
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.CompilationUnit;
+import java.util.List;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
+import java.util.ArrayList;
+import java.util.Optional;
+import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.FieldDeclaration;
+
+public class ProgramSize
+{
+    private List<CompilationUnit> cuList;
+    private int linesOfCode;
+    private int sourceLineOfCode;
+    private int statementsOfCode;
+    private int statementsOfCodeInClass;
+    private List<Integer> statementsPerClass;
+    private String parsingThis;
+    private List<String> paths;
+    private String results;
+
+    public ProgramSize(List<CompilationUnit> newCuList, String curentClass)
+    {
+        cuList = newCuList;
+        sourceLineOfCode = 0;
+        linesOfCode = 0;
+        statementsOfCode = 0;
+        statementsOfCodeInClass = 0;
+        parsingThis = curentClass;
+        statementsPerClass = new ArrayList<Integer>();
+        paths = new ArrayList<String>();
+        results = "";
+
+        Optional<String> ptn;
+        Optional<PackageDeclaration> pd;
+        for(CompilationUnit c: cuList)
+        {
+            ptn = c.getPrimaryTypeName();
+            pd = c.getPackageDeclaration();
+            if (pd.isPresent()) {
+                if(ptn.isPresent())
+                {
+                    paths.add(pd.get().getName().toString()+"."+ptn.get());
+                } else {
+                    paths.add("null");
+                }
+            } else {
+                if(ptn.isPresent())
+                {
+                    if(!paths.contains(ptn.get()))
+                        paths.add(ptn.get());
+                    else
+                        results += "Error: When there are no packages each class must have unique class names. Multiple classes named " + ptn.get() + ".\n Size results might not be accurate.\n\n";
+                } else {
+                    paths.add("null");
+                }
+            }
+        }
+
+        for(CompilationUnit cu: cuList)
+        {
+            findStatementClass(cu);
+            findFieldVarType(cu);
+            statementsPerClass.add(statementsOfCodeInClass);
+            statementsOfCodeInClass = 0;
+        }
+    }
+
+    public String getResults()
+    {
+        results += "Program size is measured by the number of statements in a program.\n";
+        results += "Note: This metric (program size) is useful when comparing orders of magnitude. ";
+        results += "While it is debatable exactly how to measure \nprogram size, discrepancies of an order of magnitude can be clear indicators of software complexity or man-hours.\n\n";
+
+        int tableSize = 0;
+
+        if(parsingThis.equals("")) {
+            tableSize = paths.size()+2;
+        } else {
+            tableSize = 3;
+        }
+
+        final Object[][] table = new String[tableSize][];
+
+        table[0] = new String[] { "Class", "Statements Value"};
+        table[1] = new String[] { "-----", "--------------"};
+
+        int j=0;
+        for(int i = 2; i<=paths.size()+1; i++)
+        {
+            String path = paths.get(j);
+
+            if(path.contains("\\.{4}") && path.length()>40) //if path too long to print
+            {
+                String[] spliting = path.split(".");
+                path = spliting[spliting.length - 3] + "." + spliting[spliting.length - 2] + "." + spliting[spliting.length - 1];
+            }
+
+            if(parsingThis.equals("")) //if printing all classes
+            {
+                table[i] = new String[] { path, statementsPerClass.get(j).toString()};
+            } else { //if printing one class
+                if(path.contains(parsingThis)) //only print result if path contains class, excludes packages
+                {
+                    table[2] = new String[] { path, statementsPerClass.get(j).toString()};
+                    break;
+                }
+            }
+
+            j++;
+        }
+
+        for (final Object[] row : table) {
+            results += String.format("%40s%40s\n", row);;
+        }
+
+        results += "\n";
+        String grammar = "This result is";
+
+        results += "Statements in all classes: ";
+        results += statementsOfCode + "\n";
+        if(!parsingThis.equals(""))
+        {
+            results += "The number of statements in the current class is shown in the above table.\n";
+            grammar = "These results are";
+        }
+
+        results += "Note: " + grammar + " either good or bad depending on what the program is doing.";
+
+        return results;
+    }
+
+    private void findStatementClass(CompilationUnit cu) {
+        cu.findAll(Statement.class).forEach(s -> { //for each method call
+            statementsOfCode++;
+            statementsOfCodeInClass++;
+        });
+    }
+
+    private void findFieldVarType(CompilationUnit cu) {
+        findVarTypeHelper(cu, FieldDeclaration.class);
+    }
+
+    private <T extends Node> void findVarTypeHelper(CompilationUnit cu, Class<T> cType) {
+        cu.findAll(cType).forEach(v -> { //for each declaration
+            statementsOfCode++;
+            statementsOfCodeInClass++;
+        });
+    }
+}
